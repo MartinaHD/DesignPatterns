9522ea6902ac474d9130aafe09600d4702d2e272
DesignPatterns <MartinaHD@example.com>
2024-03-13 21:50:27 +0200

    first commit

---
+++ BuilderPatternCar/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+using BuilderPatternCar.Builder;
+using BuilderPatternCar.ConcreteBuilder;
+using BuilderPatternCar.Director;
+
+class Program
+{
+    static void Main()
+    {
+        CarBuilder builder;
+
+        Specifications specifications = new Specifications();
+
+        builder = new ToyotaBuilder();
+        specifications.Construct(builder);
+        builder.Car.Show();
+
+        builder = new HondaBuilder();
+        specifications.Construct(builder);
+        builder.Car.Show();
+
+        builder = new PeugeotBuilder();
+        specifications.Construct(builder);
+        builder.Car.Show();
+
+        Console.ReadKey();
+    }
+}
